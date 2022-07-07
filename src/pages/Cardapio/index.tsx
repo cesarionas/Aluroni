0767f26bd10aad95789b6b0ae907cdd592a54694
cesarionas <vini.cesario.n@gmail.com>
2022-07-07 19:55:53 -0300

    Filtro

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -2,6 +2,7 @@ import styles from "./Cardapio.module.scss";
 import { ReactComponent as Logo } from "assets/logo.svg";
 import Buscador from "./Buscador";
 import { useState } from "react";
+import Filtros from "./Filtros";
 
 export default function Cardapio() {
   const [busca, setBusca] = useState("");
@@ -19,6 +20,9 @@ export default function Cardapio() {
         <section className={styles.cardapio}>
           <h3 className={styles.cardapio__titulo}>Cardápio</h3>
           <Buscador busca={busca} setBusca={setBusca} />
+          <div className={styles.cardapio__filtros}>
+            <Filtros />
+          </div>
         </section>
     </main>
   )
