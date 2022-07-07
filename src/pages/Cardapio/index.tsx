291f5b1987c2967b924d73a27752569033d7ed6e
cesarionas <vini.cesario.n@gmail.com>
2022-07-07 18:28:13 -0300

    header e variaveis sass

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,17 @@
+import styles from "./Cardapio.module.scss";
+import { ReactComponent as Logo } from "assets/logo.svg";
+
+export default function Cardapio() {
+  return (
+    <main>
+      <nav className={styles.menu}>
+        <Logo />
+      </nav>
+      <header className={styles.header}>
+          <div className={styles.header__text}>
+            A casa do código e da massa
+          </div>
+        </header>
+    </main>
+  )
+}
